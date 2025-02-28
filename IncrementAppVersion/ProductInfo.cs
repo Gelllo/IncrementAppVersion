26a45bdc4e5c1da3ee88f9e83498e876086f9325
Gelu-Florin Boldureanu <geluboldureanu@gmail.com>
2025-02-28 20:06:08 +0200

    Added working version, giving an argument, it increase the release directly in file, still needs refactoring - WIP

---
+++ IncrementAppVersion/ProductInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementAppVersion
+{
+    public static class ProductInfo
+    {
+        public static readonly string ProductVersion = "1.0.1.0";
+    }
+}
--- IncrementAppVersion/Program.cs
+++ IncrementAppVersion/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using IncrementAppVersion.Enums;
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -9,12 +11,41 @@
         Console.WriteLine($"Version type: {input}");
 
         //Read file contents
-
+        string filePath = "";
+        string contents = File.ReadAllText(filePath);
         //Parse input into enum type
+
+        if(Enum.TryParse(input, ignoreCase:true, out ReleaseType releaseType))
+        {
+            Console.WriteLine($"Release type: {releaseType}");
+        }
+        else
+        {
+            throw new InvalidCastException("Invalid release type");
+        }
 
         //Based on input type increment version
 
+        string currentReleaseString = "1.0.0.0";
+
+        uint[] releaseParsed = currentReleaseString.Split('.').Select(uint.Parse).ToArray();
+
+        var index = (int)releaseType;
+
+        releaseParsed[index]++;
+        while (index < 3)
+        {
+            releaseParsed[++index] = 0;
+        }
+
+        string newVersion = string.Join(".", releaseParsed);
+
+        Console.WriteLine("New version: " + newVersion);
         //Write to file contents
+
+        string newContents = contents.Replace(currentReleaseString, newVersion);
+
+        File.WriteAllText(filePath, newContents);
 
         Console.ReadKey();
     }
